1da7098fe826a015bccebfd68b8fb8a9524a427f
Magui Ripa <margaritaripa@hotmail.com>
2022-06-28 20:25:56 -0300

    hover hecho

--- calculadora.py
+++ calculadora.py
@@ -4,15 +4,15 @@ from turtle import bgcolor, right
 
 
 ventana = Tk()
-ventana.geometry("250x215")
+ventana.geometry("300x225")
 ventana.config(bg="#ff99c8")
 ventana.title("Calculadora Magui")
 
-mainLabel = Label(text = "Calculadora de Magui",bg="#ff99c8",font= ("Comic sens MC" , "20", "bold"),fg ="#E4C1F9")
+mainLabel = Label(text = "Calculadora de Magui",bg="#fde2e4",font= ("Comic sens MC" , "18", "bold"),fg ="#ff5d8f")
 mainLabel.grid(row = 0,columnspan = 6, sticky = E+W)
 
 #Pantalla de ingreso
-display = Entry(ventana, bg= "#fcf6bd", relief="groove", font= ("Comic sens MC" , "16", "bold italic"), fg ="#E4C1F9", justify= "right")
+display = Entry(ventana, bg= "#fcf6bd", relief="groove", font= ("Comic sens MC" , "16", "bold italic"), fg ="#ff686b", justify= "right")
 display.grid(row =1, columnspan = 6, sticky = E+W)
 display.bind("<Enter>")
 
@@ -77,30 +77,30 @@ Button3 = HoverButton (ventana, text ="3", activebackground="#fcf6bd", bg="#ff99
 
 Button4 = HoverButton (ventana, text ="4", activebackground= "#fcf6bd", bg= "#ff99c8", command=lambda: obtenerNum(4)).grid(row = 3, column = 0,sticky = E+W)
 Button5 = HoverButton (ventana, text ="5", activebackground= "#fcf6bd", bg= "#ff99c8", command=lambda: obtenerNum(5)).grid(row = 3, column = 1,sticky = E+W)
-Button6 = HoverButton (ventana, text ="6",command=lambda: obtenerNum(6)).grid(row = 3, column = 2,sticky = E+W)
+Button6 = HoverButton (ventana, text ="6",bg="#ff99c8", activebackground="#fcf6bd",command=lambda: obtenerNum(6)).grid(row = 3, column = 2,sticky = E+W)
 
-Button7 = HoverButton (ventana, text ="7",command=lambda: obtenerNum(7)).grid(row = 4, column = 0,sticky = E+W)
-Button8 = HoverButton (ventana, text ="8",command=lambda: obtenerNum(8)).grid(row = 4, column = 1,sticky = E+W)
-Button9 = HoverButton (ventana, text ="9",command=lambda: obtenerNum(9)).grid(row = 4, column = 2,sticky = E+W)
+Button7 = HoverButton (ventana, text ="7",bg="#ff99c8", activebackground="#fcf6bd",command=lambda: obtenerNum(7)).grid(row = 4, column = 0,sticky = E+W)
+Button8 = HoverButton (ventana, text ="8",bg="#ff99c8", activebackground="#fcf6bd",command=lambda: obtenerNum(8)).grid(row = 4, column = 1,sticky = E+W)
+Button9 = HoverButton (ventana, text ="9",bg="#ff99c8", activebackground="#fcf6bd",command=lambda: obtenerNum(9)).grid(row = 4, column = 2,sticky = E+W)
 
-Button0 = HoverButton (ventana, text ="0",command=lambda: obtenerNum(0)).grid(row = 5, column = 1,sticky = E+W)
+Button0 = HoverButton (ventana, text ="0",bg="#ff99c8", activebackground="#fcf6bd",command=lambda: obtenerNum(0)).grid(row = 5, column = 1,sticky = E+W)
 
 #botones para operar
-ButtonAC = HoverButton (ventana, text ="AC", command= lambda: clearDisplay()).grid(row = 5, column = 0,sticky = E+W)
-ButtonCom = HoverButton (ventana, text =",", command = lambda: operacion(".")).grid(row = 5, column = 2,sticky = E+W)
+ButtonAC = HoverButton (ventana, text ="AC",bg="#E4C1F9", activebackground="#fcf6bd", command= lambda: clearDisplay()).grid(row = 5, column = 0,sticky = E+W)
+ButtonCom = HoverButton (ventana, text =",",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion(".")).grid(row = 5, column = 2,sticky = E+W)
 
-ButtonMas = HoverButton (ventana, text ="+", command = lambda: operacion("+")).grid(row = 3, column = 3,sticky = E+W)
-ButtonMenos = HoverButton (ventana, text ="-", command = lambda: operacion("-")).grid(row = 4, column = 3,sticky = E+W)
-ButtonDiv = HoverButton (ventana, text ="/", command = lambda: operacion("/")).grid(row = 5, column = 3,sticky = E+W)
-ButtonPor = HoverButton (ventana, text ="*", command = lambda: operacion("*")).grid(row = 5, column = 3,sticky = E+W)
-ButtonResto = HoverButton (ventana, text ="%", command = lambda: operacion("%")).grid(row = 5, column = 4,sticky = E+W)
-ButtonParA = HoverButton (ventana, text ="(", command = lambda: operacion("(")).grid(row = 6, column = 3,sticky = E+W)
-ButtonParC = HoverButton (ventana, text =")", command = lambda: operacion(")")).grid(row = 6, column = 4,sticky = E+W)
+ButtonMas = HoverButton (ventana, text ="+",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("+")).grid(row = 3, column = 3,sticky = E+W)
+ButtonMenos = HoverButton (ventana, text ="-",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("-")).grid(row = 4, column = 3,sticky = E+W)
+ButtonDiv = HoverButton (ventana, text ="/",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("/")).grid(row = 5, column = 3,sticky = E+W)
+ButtonPor = HoverButton (ventana, text ="*",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("*")).grid(row = 5, column = 3,sticky = E+W)
+ButtonResto = HoverButton (ventana, text ="%",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("%")).grid(row = 5, column = 4,sticky = E+W)
+ButtonParA = HoverButton (ventana, text ="(",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("(")).grid(row = 6, column = 3,sticky = E+W)
+ButtonParC = HoverButton (ventana, text =")",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion(")")).grid(row = 6, column = 4,sticky = E+W)
 
-ButtonFle = HoverButton (ventana, text ="⬅", command = lambda: undo()).grid(row = 2, column = 3, columnspan = 2,sticky = E+W) #borrar numero
-ButtonExp = HoverButton (ventana, text ="exp", command = lambda: operacion("**")).grid(row = 3, column = 4,sticky = E+W)
-ButtonExp2 = HoverButton (ventana, text ="^2", command = lambda: operacion("**2")).grid(row = 4, column = 4,sticky = E+W)
-ButtonIgual = HoverButton (ventana, text ="=", relief = "raised", command= lambda: calcular()).grid(row = 6, column = 0, columnspan = 3,sticky = E+W)
+ButtonFle = HoverButton (ventana, text ="⬅",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: undo()).grid(row = 2, column = 3, columnspan = 2,sticky = E+W) #borrar numero
+ButtonExp = HoverButton (ventana, text ="exp",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("**")).grid(row = 3, column = 4,sticky = E+W)
+ButtonExp2 = HoverButton (ventana, text ="^2",bg="#E4C1F9", activebackground="#fcf6bd", command = lambda: operacion("**2")).grid(row = 4, column = 4,sticky = E+W)
+ButtonIgual = HoverButton (ventana, text ="=",bg="#D0F4DE", activebackground="#fcf6bd", relief = "raised", command= lambda: calcular()).grid(row = 6, column = 0, columnspan = 3,sticky = E+W)
 
 
 
